cf4f22491b7472f0afba6b72f352a2d33adeb537
Yanis2020 <amimeuryanis@gmail.com>
2020-12-28 21:40:02 -0600

    second commit

--- Amazon/src/main/java/com/amazon/pages/HomePage.java
+++ Amazon/src/main/java/com/amazon/pages/HomePage.java
@@ -3,6 +3,7 @@ package com.amazon.pages;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 
 public class HomePage {
 
@@ -13,7 +14,7 @@ public class HomePage {
 
     public void setAmazonLogo() {
         LOGGER.info("see if the amazon logo is displayed");
-        amazonLogo.isDisplayed();
+        Assert.assertTrue(amazonLogo.isDisplayed());
     }
 
 }
